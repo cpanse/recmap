aca33c182fd89a759fbe0a8f1a84189b714bf3d5
Christian Panse <cp@fgcz.ethz.ch>
2016-05-23 21:26:29 +0200

    cosmetics

--- tests/testthat/test-recmap.R
+++ tests/testthat/test-recmap.R
@@ -7,11 +7,11 @@ test_that("input 2x2 checker board", {
   
  input.map <- data.frame(x = c(1, 1, 2, 2), 
   	y = c(1, 2, 1, 2), 
-	dx = rep(0.5, 4), 
-	dy = rep(0.5, 4), 
-	z = c(4, 1, 1, 4), 
-	name = c('a1', 'a2', 'b1', 'b2'))
-
+  dx = rep(0.5, 4), 
+  dy = rep(0.5, 4), 
+  z = c(4, 1, 1, 4), 
+  name = c('a1', 'a2', 'b1', 'b2'))
+ 
   output.recmap <- recmap(input.map)
 
   expect_true(sum(abs(c(0.7250889, 1.0413167, 2.0000000, 2.0000000) - output.recmap$x)) < 1E-5)
