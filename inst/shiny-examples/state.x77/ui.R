7d198f62ca7b4f82b47d7a059246167593ca810d
Christian Panse <cp@fgcz.ethz.ch>
2017-03-14 09:33:08 +0100

    initial shiny state.x77 app

---
+++ inst/shiny-examples/state.x77/ui.R
@@ -0,0 +1,40 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel(paste("state.x77 data using recmap version", packageVersion('recmap'))),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      selectInput('area', 'area', colnames(state.x77)),
+      selectInput('color', 'color', colnames(state.x77)),
+      p('Metaheuristic (GA):'),
+      sliderInput("GApopulation", "GApopulation", 1, 10, 1),
+      numericInput("GAmaxiter", "GAmaxiter", 10),
+      numericInput("GArun", "GArun", 10),
+      sliderInput("GApmutation", "GApmutation", 0, 1, 0.2)
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+     
+      p('please wait some seconds until the cartogram is computed.'),
+      plotOutput("distPlot", hover = hoverOpts(id = "plot_hover", delayType = "throttle")),
+      #h4("Mouse over Information"),
+      tableOutput("plot_hoverinfo"),
+      p('compute your own cartogram with', a('recmap.', 
+                href='https://CRAN.R-project.org/package=recmap')),
+      p('the colors are the default R heat.colors (red is low; white is high)'),
+      img(src='heat.png', align = "left", width=200)
+      
+    )
+  )
+))
