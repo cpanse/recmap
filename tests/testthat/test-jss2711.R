5b48d4048c3e249e61e113c827e0b3442083179d
Christian Panse <cp@fgcz.ethz.ch>
2018-03-25 10:18:21 +0200

    text cosmetics

--- tests/testthat/test-jss2711.R
+++ tests/testthat/test-jss2711.R
@@ -2,7 +2,7 @@
 
 context("Test jss2711")
 
-test_that("test recmapGA on a 4x4 checkerboard (jss2711 paper Figure 7)", {
+test_that("reproducibility check for recmapGA using a 4x4 checkerboard (jss2711 paper Figure 7)", {
   
   seeds <- c(1, 1, 2, 2, 3, 3)
   
@@ -25,7 +25,7 @@ test_that("test recmapGA on a 4x4 checkerboard (jss2711 paper Figure 7)", {
   expect_true(identical(solutions[[5]], solutions[[6]]))
 })
 
-test_that("test reproducibility of jss2711 Figures 11 (Switzerland))", {
+test_that("cross platform reproducibility check of jss2711 Figures 11 (Switzerland))", {
   
   data(jss2711)
   solutions.best <- Switzerland$solution
@@ -38,7 +38,7 @@ test_that("test reproducibility of jss2711 Figures 11 (Switzerland))", {
   
 })
 
-test_that("test reproducibility of jss2711 Figures 12 (SBB))", {
+test_that("cross platform reproducibility check of jss2711 Figures 12 (SBB))", {
   
   data(jss2711)
   solutions.best <- SBB$GA@solution[1, ]
@@ -51,7 +51,7 @@ test_that("test reproducibility of jss2711 Figures 12 (SBB))", {
   
 })
 
-test_that("test reproducibility of jss2711 Figures 13 (UK))", {
+test_that("cross platform  reproducibility check of jss2711 Figures 13 (UK))", {
   
   data(jss2711)
 
