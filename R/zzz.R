0c0b285879f5fcda2211c88f8264045611613885
Christian Panse <cp@fgcz.ethz.ch>
2021-05-07 12:55:10 +0200

    1.0.9

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,6 @@
 	if(interactive()){
 		version <- packageVersion('recmap')
 		packageStartupMessage("Package 'recmap' version ", version)
-		# packageStartupMessage("Type 'citation(\"recmap\")' for citing this R package in publications.")
 	  invisible()
 	}
 }
